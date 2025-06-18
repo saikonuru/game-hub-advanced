1e30eb45b3f89d277910aeccfa81ff1ca4775f49
saikonuru <sairam.konuru@gmail.com>
2025-06-18 22:33:51 +0530

    TodoForm-Optimistic Updates: enhance mutation handling with context for previous todos and update cache on success

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -3,26 +3,48 @@ import axios from "axios";
 import { useRef } from "react";
 import { Todo } from "../hooks/useTodos";
 
+interface AddTodoContext {
+  previousTodos: Todo[];
+}
+
 const TodoForm = () => {
   const queryClient = useQueryClient();
-  const addTodo = useMutation<Todo, Error, Todo>({
+  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
         .then((res) => res.data),
+
+    onMutate: (newTodo: Todo) => {
+      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
+      queryClient.setQueryData<Todo[]>(["todos"], (currentTodos) => [
+        newTodo,
+        ...(currentTodos || []),
+      ]);
+      if (ref.current) ref.current.value = "";
+      return { previousTodos };
+    },
     onSuccess: (savedTodo, newTodo) => {
-      console.log(savedTodo);
       // APPROACH 1 - Does't work with jsonplaceholder
       //   queryClient.invalidateQueries({
       //     queryKey: ["todos"],
       //   });
 
       // APPROACH 2 : updating the data in the cache
-      queryClient.setQueryData<Todo[]>(["todos"], (currentTodos) => [
-        savedTodo,
-        ...(currentTodos || []),
-      ]);
-      if (ref.current) ref.current.value = "";
+      // queryClient.setQueryData<Todo[]>(["todos"], (currentTodos) => [
+      //   savedTodo,
+      //   ...(currentTodos || []),
+      // ]);
+      // if (ref.current) ref.current.value = "";
+
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
+        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+      );
+    },
+    onError: (error, newTodo, context) => {
+      if (!context) return;
+
+      queryClient.setQueryData<Todo[]>(["todos"], context.previousTodos);
     },
   });
   const ref = useRef<HTMLInputElement>(null);
