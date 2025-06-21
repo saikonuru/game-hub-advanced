8f82336bd6262b96aa32664125cf34b36ec95707
saikonuru <sairam.konuru@gmail.com>
2025-06-21 22:22:41 +0530

    Fix initialData assignment in usePlatforms hook for consistent data fetching

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
+import platforms from "../data/platforms";
 import apiClient from "../services/api-client";
 import { FetchResponse } from "./useData";
+
 interface Platform {
   id: number;
   name: string;
@@ -17,7 +19,7 @@ const usePlatforms = () =>
         .get<FetchResponse<Platform>>("/platforms/lists/parents")
         .then((res) => res.data),
     staleTime: 24 * 60 * 60 * 1000, // 24-HR
-    //initialData: { count: platforms.length, results: platforms },
+    initialData: { count: platforms.length, results: platforms },
   });
 
 export default usePlatforms;
