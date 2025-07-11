9494c5c8fbefec170e3938f5ce9bf2769a563c57
saikonuru <sairam.konuru@gmail.com>
2025-07-11 10:40:16 +0530

    Refactor imports to use entities for Game, Genre, and Platform; remove redundant type definitions

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,12 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
 import genres from "../data/genres";
 import ApiClient from "../services/api-client";
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
-
+import { Genre } from "../entities/Genre";
 const apiClient = new ApiClient<Genre>("/genres");
 
 const useGenres = () =>
