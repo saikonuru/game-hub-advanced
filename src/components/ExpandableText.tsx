cc93bc983733d94a6b3c48f4de07222b4ec0cbf1
saikonuru <sairam.konuru@gmail.com>
2025-07-11 11:07:42 +0530

    Replace Text component with ExpandableText for game description in GameDetailPage

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,35 @@
+import { Button, Text } from "@chakra-ui/react";
+import { useState } from "react";
+interface Props {
+  children: string;
+}
+const ExpandableText = ({ children }: Props) => {
+  const limit = 300;
+
+  if (!children) null;
+
+  if (children.length <= limit) return <Text>{children}</Text>;
+
+  const [expanded, setExpanded] = useState(false);
+
+  const summary = expanded ? children : `${children.substring(0, limit)}...`;
+
+  return (
+    <Text>
+      {summary}
+      <Button
+        size="xs"
+        marginLeft={1}
+        fontWeight="bold"
+        colorScheme="yellow"
+        onClick={() => {
+          setExpanded(!expanded);
+        }}
+      >
+        {expanded ? "Show Less" : "Show More"}{" "}
+      </Button>
+    </Text>
+  );
+};
+
+export default ExpandableText;
