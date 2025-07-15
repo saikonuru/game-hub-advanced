2d48d26e31ef8df6912b00bb632e0c3109e28930
saikonuru <sairam.konuru@gmail.com>
2025-07-15 22:12:22 +0530

    Refrector entities

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,8 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
 import genres from "../data/genres";
+import Genre from "../entities/Genre";
 import ApiClient from "../services/api-client";
-import { Genre } from "../entities/Genre";
 const apiClient = new ApiClient<Genre>("/genres");
 
 const useGenres = () =>
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
 import platforms from "../data/platforms";
-import { Platform } from "../entities/Platform";
+import Platform from "../entities/Platform";
 import ApiClient from "../services/api-client";
 
 const apiClient = new ApiClient<Platform>("/platforms/lists/parents");
